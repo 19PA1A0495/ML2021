f57257cc3c4d83e350c5a4dc509074fbb5cdc0b0
ksumanjali478 <73094996+ksumanjali478@users.noreply.github.com>
2020-10-19 18:48:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
+"""
+This is a program for chatbot to give suggestions for online courses.
+1. The chatbot name is boomer.
+2. The bommer will start with a greeting and self intro and ask for name of the person .
+3. The bommer give suggestions of courses for 10th and secondary standard students.
+4. Bommer will calculate ur age .
+5. Bommer will crack a joke and finally the bot ends.
+"""
 import datetime
 import random
 
@@ -98,10 +106,72 @@ def stream():
         print("3. Indian history")
         print("4. Harappan civilization")
         print("---------------------------------------------")
+def suggestions():
+    twelth_stream = stream()
+    if twelth_stream == 1:
+        print("In this pandemic situation, we have to utlize this time for improving knowledge.")
+        print("I will suggest you some courses....")
+        print("For MPC, you an prefer courses like: ")
+        print("1. Basics of Programming")
+        print("2. Basis of Electronics")
+        print("3. Intro to Designing")
+        print("4. Basics of Web development")
+        print("--------------------------------------")
+    elif twelth_stream == 2:
+        print("In this pandemic situation, we have to utlize this time for improving knowledge.")
+        print("I will suggest you some courses....")
+        print("For BiPC, you can prefer courses like: ")
+        print("1. Know about Anatomy")
+        print("2. Intro to Bio Chemistry")
+        print("3. Agriculture Development")
+        print("4. Intro to Botany")
+        print("--------------------------------------")
+    elif twelth_stream == 3:
+        print("In this pandemic situation, we have to utlize this time for improving knowledge.")
+        print("I will suggest you some courses....")
+        print("For CEC, you can prefer courses like: ")
+        print("1. Public policy analysis")
+        print("2. Society and media")
+        print("3. Numerical analysis")
+        print("4. Corporate law")
+        print("--------------------------------------")
+    elif twelth_stream == 4:
+        print("In this pandemic situation, we have to utlize this time for improving knowledge.")
+        print("I will suggest you some courses....")
+        print("For HEC, you can prefer courses like: ")
+        print("1. Applied business analytics")
+        print("2. Enterpreneur strategies")
+        print("3. Business model innovations")
+        print("4. Strategic management of innovations")
+        print("---------------------------------------")
+    elif twelth_stream == 5:
+        print("In this pandemic situation, we have to utlize this time for improving knowledge.")
+        print("I will suggest you some courses....")
+        print("For Diploma, you can prefer courses like: ")
+        print("1. Programming basics")
+        print("2. Foundations of data structures")
+        print("3. Basic electronics")
+        print("4. Analog circuits")
+        print("-----------------------------------------")
         
 def joke():
     jokes = ["I ate a clock yesterday, it was very time-consuming."," I failed math so many times at school, I can’t even count.","I want to die peacefully in my sleep, like my grandfather… Not screaming and yelling like the passengers in his car.","Most people are shocked when they find out how bad I am as an electrician.","Just burned 2,000 calories. That’s the last time I leave brownies in the oven while I nap."]
     print(random.choice(jokes))
+    
+def age_calculation():
+    today_date = datetime.date.today()
+    try:
+        year = int(input("Enter your birth year: "))
+        month = int(input("Enter your birth month: "))
+        date = int(input("Enter your birth date: "))
+        birth_day = datetime.date(year,month,date)
+        age = today_date.year - birth_day.year
+        if age < 0:
+            print("Enter valid date")
+        else:
+            return age
+    except Exception:
+        print("Enter valid input")
     
 def boomer():
     greeting()
